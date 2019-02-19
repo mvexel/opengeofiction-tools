48ad2d5bc0df0e25523857200bda6633ad42a736
Martijn van Exel <m@rtijn.org>
2019-02-19 11:21:31 -0700

    Adding JOSM start script

---
+++ josm/start-ogf-josm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Set JOSM location
+export JOSM_JAR_PATH="$HOME/bin/josm-tested.jar"
+
+# Set location of JOSM environment
+export JOSM_HOME="$HOME/ogf/josm/"
+
+# Start JOSM with this environment
+java \
+    -Djosm.home=$JOSM_HOME \
+    -Djosm.userdata=$JOSM_HOME/userdata \
+    -Djosm.cache=$JOSM_HOME/cache \
+    -Djosm.pref=$JOSM_HOME/pref \
+    -Xmx4G \
+    -jar $JOSM_JAR_PATH
